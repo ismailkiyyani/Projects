31ad3104055f7818c500ef50b1c98b6a7e2a8b3e
ikayani <ismailkayani@teresol.com>
2024-08-09 17:33:35 +0500

    In progresss

--- script.js
+++ script.js
@@ -27,12 +27,15 @@ let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 let btn = document.getElementById('todoButton');
+let notesbtn = document.getElementById('notesButton');
+let textarea = document.getElementById('wrapperBox');
 
 // Get the <span> element that closes the modal
 let span = document.getElementById('close');
 let header = document.getElementById('header');
 
 let mainBox = document.getElementById('mainBox');
+let backButton = document.getElementById('backButton');
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
@@ -41,18 +44,30 @@ btn.onclick = function () {
 
   mainBox.style.display = 'none';
 };
+notesbtn.onclick = function () {
+  textarea.style.display = 'block';
+  header.style.display = 'none';
 
+  mainBox.style.display = 'none';
+};
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   console.log('close');
 
   modal.style.display = 'none';
 };
+backButton.onclick = function () {
+  header.style.display = 'block';
+  mainBox.style.display = 'flex';
+  textarea.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
+    header.style.display = 'block';
+    mainBox.style.display = 'flex';
   }
 };
 
